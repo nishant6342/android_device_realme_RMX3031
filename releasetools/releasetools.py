fc19479e294a38d323833f25c20d6bb503c36ae6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-20 00:04:21 +0530

    RMX3031: releasetools: Unmount /apex if mounted
     * some recovery builds have some retarded apex script
       which fails when apexes are flattened, and leave
       /apex mounted, which causes system partition to be
       busy and rom flash fails
    
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -21,6 +21,7 @@ def FullOTA_InstallBegin(info):
   data = info.input_zip.read("RADIO/dynamic-remove-oplus")
   common.ZipWriteStr(info.output_zip, "dynamic-remove-oplus", data)
   info.script.AppendExtra('update_dynamic_partitions(package_extract_file("dynamic-remove-oplus"));')
+  info.script.AppendExtra("ifelse(is_mounted(\"/apex\"), unmount(\"/apex\"));")
   return
 
 def FullOTA_InstallEnd(info):
