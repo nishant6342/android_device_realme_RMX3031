a2c69077eab280bf6c10ae7c4353ac6a0937defb
TTTT555 <albukerko@gmail.com>
2021-12-24 18:29:59 +0530

    RMX3031: Add camera aux package list
    - Required for third party apps to use their built in camera e.g Telegram
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I22ab565d7d75e278faf4600cd5aecec59164bf8f
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -6,6 +6,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1
 
+# Camera
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera \
+    vendor.camera.aux.packageblacklist=org.telegram.messenger,com.microsoft.teams,com.discord
+
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.realme.lockstate=0
