969f26e8fb25bf0ba63b2a9ebf34b90e83127331
Nishant Kumar <www.rajsonu13@gmail.com>
2022-02-20 09:08:08 +0530

    RMX3031:[Properties]: Minor changes
    * Change PRODUCT_SYSTEM_DEFAULT_PROPERTIES to PRODUCT_SYSTEM_PROPERTIES as it is highly discouraged and will be deprecated by google
    
    * Change PRODUCT_PROPERTY_OVERRIDES to PRODUCT_PRODUCT_PROPERTIES as init reads /product/build.prop after /vendor/build.prop so we can override props set by /vendor/build.prop by specifying props in /product/build.prop
    
    Reference: - https://android.googlesource.com/platform/build/+/master/Changes.md#changes-in-system-properties-settings
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,6 +1,6 @@
 # Bluetooth
-PRODUCT_PROPERTY_OVERRIDES += \
-     persist.bluetooth.bluetooth_audio_hal.disabled=true
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.bluetooth.bluetooth_audio_hal.disabled=true
 
 # Blurs
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -11,7 +11,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     persist.cabc_profile=2
 
 # Camera
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera \
     vendor.camera.aux.packageblacklist=org.telegram.messenger,com.microsoft.teams,com.discord
 
@@ -21,11 +21,11 @@ PRODUCT_SYSTEM_PROPERTIES += \
     pm.dexopt.bg-dexopt=everything
 
 # Fingerprint
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.realme.lockstate=0
 
 # IMS
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
@@ -35,19 +35,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.device_config.runtime_native_boot.iorap_readahead_enable=true
 
 # NFC
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.nfc.disPowerSave=1 \
     persist.nfc.smartcard.recorder.enable=true \
     persist.sys.nfc.default=on
 
 # OMX
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0 \
     debug.stagefright.ccodec=0
 
 # Performance
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     ro.mtk_perf_simple_start_win=1 \
     ro.mtk_perf_fast_start_win=1 \
     ro.mtk_perf_response_time=1
@@ -57,7 +57,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.control_privapp_permissions=log
 
 # Surfaceflinger
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.latch_unsignaled=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
@@ -69,17 +69,17 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
 # Zygote
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
 
 # ZRAM
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.first_wb_delay_mins=1440 \
     ro.zram.periodic_wb_delay_hours=24
 
 # Encryption
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.crypto.dm_default_key.options_format.version=2 \
     ro.crypto.volume.filenames_mode=aes-256-cts \
     ro.crypto.volume.metadata.method=dm-default-key \
