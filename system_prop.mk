fa88ce277a4e6fcedf95c416f884184e0c203fe9
ManshuTyagi <himanshut567@gmail.com>
2022-05-04 03:27:10 +0530

    RMX3031: Cleanup some props and blobs
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: I3b6056d38a83a8060085a74126776df6dea1e49b

--- system_prop.mk
+++ system_prop.mk
@@ -1,36 +1,8 @@
-# Bluetooth
-PRODUCT_PROPERTY_OVERRIDES += \
-     persist.bluetooth.bluetooth_audio_hal.disabled=true
-
-# Blurs
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.surface_flinger.supports_background_blur=1
-
-# Fingerprint
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.boot.realme.lockstate=0
-
 # IMS
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
-
-# Iorap
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.device_config.runtime_native_boot.iorap_readahead_enable=true
-
-# NFC
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.sys.nfc.disPowerSave=1 \
-    persist.nfc.smartcard.recorder.enable=true \
-    persist.sys.nfc.default=on
-
-# OMX
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    debug.stagefright.omx_default_rank.sw-audio=1 \
-    debug.stagefright.omx_default_rank=0 \
-    debug.stagefright.ccodec=0
 
 # Performance
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -48,10 +20,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
-
-# Zygote
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.device_config.runtime_native.usap_pool_enabled=true
 
 # ZRAM
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
