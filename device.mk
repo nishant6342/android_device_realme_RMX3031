bb305b1bd40284d7a00a8941133fe59cdbc597cc
Nishant Kumar <www.rajsonu13@gmail.com>
2022-05-03 16:58:29 +0530

    RMX3031: Drop mediatek ims BOOT_JARS

--- device.mk
+++ device.mk
@@ -154,16 +154,6 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 PRODUCT_PACKAGES += \
     libshim_vtservice
 
-# Telephony
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
-
 PRODUCT_PACKAGES += \
     ImsServiceBase
 
