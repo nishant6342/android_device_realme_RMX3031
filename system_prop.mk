e5d92a17edce298a82474840d4973d0b4f71f1b1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-18 06:17:49 +0530

    RMX3031: Set surfaceflinger props to auto change refresh rate based on content
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -61,7 +61,12 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.sf.latch_unsignaled=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+    debug.sf.frame_rate_multiple_threshold=120 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.set_idle_timer_ms=500 \
+    ro.surface_flinger.set_touch_timer_ms=750 \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
 # Zygote
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
