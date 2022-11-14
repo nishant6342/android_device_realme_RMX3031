f99ce6665bc65aa855eef1987b02c7224992696e
Volodymyr Zhdanov <wight554@gmail.com>
2022-11-14 09:50:09 +0530

    RMX3031: Disable SF configstore * It's finally deprecated in R * Ship disable_configstore because we have API level 30
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: I3efd06f1e4ad46850d0bd8de75011229f6763126

--- device.mk
+++ device.mk
@@ -255,7 +255,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0.vendor \
     android.hardware.graphics.composer@2.2-service \
     android.hardware.memtrack@1.0-service \
-    android.hardware.memtrack@1.0-impl
+    android.hardware.memtrack@1.0-impl \
+    disable_configstore
 
 # GNSS
 PRODUCT_PACKAGES += \
