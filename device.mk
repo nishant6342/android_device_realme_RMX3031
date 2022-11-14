a5bd5514af4f36f0055b9c897b9d17180b2d9af0
ManshuTyagi <himanshut567@gmail.com>
2022-11-14 09:21:39 +0530

    RMX3031: Build requied libraries for gnss
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: I506315d9f791e6cbde0d20eb7685421306cb13f1

--- device.mk
+++ device.mk
@@ -257,6 +257,16 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.memtrack@1.0-impl
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss.measurement_corrections@1.0.vendor \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    android.hardware.gnss@1.0.vendor \
+    android.hardware.gnss@1.1.vendor \
+    android.hardware.gnss@2.0.vendor \
+    android.hardware.gnss@2.1.vendor
+
 # RcsService
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
