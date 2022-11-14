eaa3daa06fe41bb71161e87d59309ba9f38a0516
LuK1337 <priv.luk@gmail.com>
2022-11-14 11:14:10 +0530

    RMX3031: sensors: Import sensors/2.0/multihal
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: I7ae9a113eddbc97ca5dbfaeda75418ed2f96a8bb

--- device.mk
+++ device.mk
@@ -294,6 +294,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service-multihal.RMX3031 \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.0.vendor \
     android.hardware.sensors@2.1.vendor \
