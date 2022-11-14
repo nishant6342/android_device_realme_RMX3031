0243d48068a2806a65f76b8cf77917ef6183e221
Vaisakh Murali <mvaisakh@statixos.com>
2022-11-14 14:13:11 +0530

    RMX3031: configs: power: Move power and perf configs to device tree This would help changing perf and power HAL parameters in userspace.
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: I4d19ce099b06c1ba2e6a2c3b41b39e32af3c4eb1

--- device.mk
+++ device.mk
@@ -333,6 +333,11 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/power_app_cfg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_app_cfg.xml \
+    $(LOCAL_PATH)/configs/power/powercontable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powercontable.xml \
+    $(LOCAL_PATH)/configs/power/powerscntbl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerscntbl.xml
+
 # Secure Element
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0.vendor \
