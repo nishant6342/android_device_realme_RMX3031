30f9a6d3b2123c32e30696c49dc0fe371531bd21
ManshuTyagi <himanshut567@gmail.com>
2022-11-14 11:45:27 +0530

    RMX3031: Build libsensorndkbridge from source
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: I6be73526d0e217135f8470b18ae32945635eaafa

--- device.mk
+++ device.mk
@@ -299,7 +299,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0.vendor \
     android.hardware.sensors@2.1.vendor \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
-    android.frameworks.sensorservice@1.0.vendor
+    android.frameworks.sensorservice@1.0.vendor \
+    libsensorndkbridge
 
 # Keymaster
 PRODUCT_PACKAGES += \
