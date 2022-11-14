eb3b6a4121d6c644cfade72c09f3988bd634dc7f
ManshuTyagi <himanshut567@gmail.com>
2022-11-14 09:43:34 +0530

    RMX3031: Build required libraries for USB
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: Icc06f7461e2ad1cf5fa5bb4e8736a3be06981e95

--- device.mk
+++ device.mk
@@ -347,6 +347,15 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor \
     libtextclassifier_hash.vendor
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0.vendor \
+    android.hardware.usb@1.1.vendor \
+    android.hardware.usb@1.2.vendor \
+    android.hardware.usb@1.3.vendor \
+    android.hardware.usb.gadget@1.0.vendor \
+    android.hardware.usb.gadget@1.1.vendor
+
 # RcsService
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
