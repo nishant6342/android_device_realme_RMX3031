814bf35b756b58945b5b540abd035cf9054d345c
Jiyong Park <jiyong@google.com>
2022-11-19 15:09:27 +0530

    RMX3031: gnss: Remove ndk_platform backend. Use the ndk backend. The ndk_platform backend will soon be deprecated because the ndk backend can serve the same purpose. This is to eliminate the confusion about having two variants (ndk and ndk_platform) for the same ndk backend.
    Bug: 161456198
    Test: m
    
    Signed-off-by: Himanshu Tyagi <himanshut567@gmail.com>
    Change-Id: Iaeda48219f90322793f6f20e4d07b29c90d99cce

--- extract-files.sh
+++ extract-files.sh
@@ -44,6 +44,11 @@ function blob_fixup {
         vendor/lib64/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
             "$PATCHELF" --replace-needed libutils.so libutils-v32.so "$2"
             ;;
+        vendor/bin/hw/android.hardware.gnss-service.mediatek)
+            ;&
+        vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
+           "$PATCHELF" --replace-needed android.hardware.gnss-V1-ndk_platform.so android.hardware.gnss-V1-ndk.so "$2"
+            ;;
   esac
 }
 
