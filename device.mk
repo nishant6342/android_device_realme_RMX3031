4cebcfbbc4f7431fbb3af8e48b212940b5261f32
Himanshu Tyagi <himanshut567@gmail.com>
2022-11-19 17:42:53 +0530

    RMX3031: Build vendor version of android.system.keystore2-V1-ndk
    Signed-off-by: Himanshu Tyagi <himanshut567@gmail.com>
    Change-Id: I3d4df534e0fe4284a7806f92ee7252c02878ce36

--- device.mk
+++ device.mk
@@ -416,4 +416,5 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor \
+    android.system.keystore2-V1-ndk.vendor \
     android.hardware.wifi@1.0-service.RMX3031
