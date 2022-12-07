9992226f92dbe9da5c5244f9351345950e01751f
Himanshu Tyagi <himanshut567@gmail.com>
2022-12-07 09:18:14 +0530

    Revert "RMX3031: Switch to OSS Power AIDL" This reverts commit 936b8e6b3602aff42ccec941428b25cdcc221a69.
    Signed-off-by: Himanshu Tyagi <himanshut567@gmail.com>
    Change-Id: I9fbfa6f2b2efd1cc919bf5ae911c0887e2836c88

--- device.mk
+++ device.mk
@@ -313,7 +313,6 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-mediatek \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor \
