1f20ed66c0eec41a00f806102ccbd6655b766aa6
Himanshu Tyagi <himanshut567@gmail.com>
2022-12-21 11:27:23 +0530

    RMX3031: Rename libcode2_vndk to avoid conflict aosp one gives symbol error - F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.media.c2@1.2-mediatek": cannot locate symbol "_ZTVN7android5FenceE" referenced by "/vendor/lib/libcodec2_vndk.so"...
    lets use prebuilt and rename it.
    
    Signed-off-by: Himanshu Tyagi <himanshut567@gmail.com>
    Change-Id: Ic6da41abe0491c6be4548984c98c04a35e61a25c

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,7 @@ function blob_fixup {
             ;&
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
            "$PATCHELF" --replace-needed libavservices_minijail_vendor.so libavservices_minijail.so "$2"
+           "$PATCHELF" --replace-needed libcodec2_vndk.so libcodec2_vndk-mtk.so "$2"
             ;;
         vendor/bin/mtk_agpsd)
            "$PATCHELF" --replace-needed libcrypto.so libcrypto-v32.so "$2"
