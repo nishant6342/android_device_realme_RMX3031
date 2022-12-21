1e2cdc8f2730591f7be35db13d8d14bef89df0c6
Himanshu Tyagi <himanshut567@gmail.com>
2022-12-21 08:41:34 +0530

    RMX3031: Build vendor variant of DRM libs F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.4-service.widevine": library "android.hardware.drm@1.0.so" not found: needed by main executable F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.4-service.widevine": library "android.hardware.drm@1.1.so" not found: needed by main executable F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.4-service.widevine": library "android.hardware.drm@1.2.so" not found: needed by main executable F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.4-service.widevine": library "android.hardware.drm@1.3.so" not found: needed by main executable F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.4-service.widevine": library "android.hardware.drm@1.4.so" not found: needed by main executable
    Signed-off-by: Himanshu Tyagi <himanshut567@gmail.com>
    Change-Id: If0bb34ab5da7f1da10d40272d110bfe3d9629323

--- device.mk
+++ device.mk
@@ -178,6 +178,11 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.0.vendor \
+    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor \
     libdrmclearkeyplugin.vendor \
     libmockdrmcryptoplugin.vendor \
     libclearkeycasplugin.vendor \
