d3e747bf6c84d3d3f8e0cc40489e4335e36dcff8
Himanshu Tyagi <himanshut567@gmail.com>
2022-12-24 11:29:13 +0530

    Revert "RMX3031: Patch libMtkOmxVdecEx to use libui-v32" This reverts commit ed69830acb49165aa2c95ca0ba5a5a234cf1ac7d.
    we don't need to patch this after dropping prebuilt libstagefright_omx fixes the issue
    
    Signed-off-by: Himanshu Tyagi <himanshut567@gmail.com>
    Change-Id: Ib8a64a9cc8c922bc0cba1e2c952a390611f4dce9

--- device.mk
+++ device.mk
@@ -412,8 +412,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm/arch-arm-armv7-a-neon/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v32.so \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v32.so \
-    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-core/libui.so:$(TARGET_COPY_OUT_VENDOR)/lib/libui-v32.so
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v32.so
 
 # WiFi
 PRODUCT_PACKAGES += \
