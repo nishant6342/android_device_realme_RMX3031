c0dbc366f6d914de477c82ecc0a972d6d738007e
Nishant Kumar <www.rajsonu13@gmail.com>
2022-12-25 14:22:39 +0530

    RMX3031:[Vendorsetup.sh]: Fix typo
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,8 +8,8 @@ PA=packages/apps/prebuilt-apps/prebuilt-apps.mk
 if ! [ -a $PA ]; then git clone --depth=1 https://gitlab.com/nishant6342/packages_apps_prebuilt-apps packages/apps/prebuilt-apps/
 fi
 MTK_SEPOLICY=device/mediatek/sepolicy_vndr/SEPolicy.mk
-if ! [-a $MTK_SEPOLICY]; then git clone https://github.com/Project-Elixir/device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+if ! [ -a $MTK_SEPOLICY ]; then git clone https://github.com/Project-Elixir/device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 fi
 PARTS=packages/apps/RealmeParts/parts.mk
-if ! [-a $PARTS]; then git clone https://github.com/nishant6342/packages_apps_RealmeParts packages/apps/RealmeParts
+if ! [ -a $PARTS ]; then git clone https://github.com/nishant6342/packages_apps_RealmeParts packages/apps/RealmeParts
 fi
