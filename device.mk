5b0e86c8c5e49e72501ba2d6dc63a790d0b85354
Nishant Kumar <www.rajsonu13@gmail.com>
2023-02-27 13:29:17 +0530

    Revert "RMX3031: touch: Add touchscreen gestures HIDL HAL"
    This reverts commit 5cb974ea37aa0b2e6917e02b9af904dacb620baa.
    
    * No kernel source as of now to expose gesture nodes
    
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- device.mk
+++ device.mk
@@ -440,10 +440,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor \
     android.hardware.thermal@1.0-impl
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.RMX3031
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0.vendor \
