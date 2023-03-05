dee83f78c2ea442633f02d2628e941e7282e662c
Himanshu Tyagi <himanshut567@gmail.com>
2023-03-05 10:52:29 +0530

    RMX3031: overlay: Import Carrier Config overlay
    Signed-off-by: Himanshu Tyagi <himanshut567@gmail.com>
    Change-Id: Ia9a5fb51782030a1dbc2687e714d126f40ffeadb
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- device.mk
+++ device.mk
@@ -281,7 +281,8 @@ PRODUCT_PACKAGES += \
     FrameworkResOverlay \
     SystemUIOverlay \
     SettingsOverlay \
-    TelephonyOverlay
+    TelephonyOverlay \
+    CarrierConfigOverlay
 
 # Enforce RRO targets
 PRODUCT_ENFORCE_RRO_TARGETS := *
