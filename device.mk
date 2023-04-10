4a756b4dea9349065782c2a8c468bbebd1086ee3
Nishant Kumar <www.rajsonu13@gmail.com>
2023-04-10 11:26:08 +0530

    RMX3031: fix oplus sysconfig xml copy path
    * Also add oneplus gallery to whitelist
    
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- device.mk
+++ device.mk
@@ -285,7 +285,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/permissions/com.oplus.camera.unit.sdk_product.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.oplus.camera.unit.sdk_product.xml \
     $(DEVICE_PATH)/configs/permissions/oplus_camera_default_grant_permissions_list.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/oplus_camera_default_grant_permissions_list.xml \
     $(DEVICE_PATH)/configs/permissions/APU_SYS.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/APU_SYS.xml \
-    $(DEVICE_PATH)/configs/sysconfig/hiddenapi-package-whitelist-oplus-system.xml:$(TARGET_COPY_OUT_SYSTEM)/sysconfig/hiddenapi-package-whitelist-oplus-system.xml
+    $(DEVICE_PATH)/configs/sysconfig/hiddenapi-package-whitelist-oplus-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oplus-system.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
