dd55634ddfcdd03fa63062e8dec4cb98fa7aba29
bengris32 <bengris32@protonmail.ch>
2023-05-19 14:53:38 +0100

    RMX3031: Build GNSS from source
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I4490df798a5fbba4c34b3b07545551f3dd043fd5
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- device.mk
+++ device.mk
@@ -168,8 +168,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0.vendor \
     android.hardware.gnss@1.1.vendor \
     android.hardware.gnss@2.0.vendor \
-    android.hardware.gnss@2.1.vendor \
-    android.hardware.gnss-V1-ndk.vendor
+    android.hardware.gnss@2.1.vendor
+
+PRODUCT_PACKAGES += \
+    android.hardware.gnss-service.mediatek
 
 # HIDL
 PRODUCT_PACKAGES += \
