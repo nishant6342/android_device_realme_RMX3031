98c766f3d4e97af73ad01c329ec77206e34488bf
bengris32 <bengris32@protonmail.ch>
2023-06-11 18:52:17 +0530

    RMX3031: Import some sepolicies from Nashc
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,6 +178,7 @@ VENDOR_SECURITY_PATCH := 2023-03-05
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS := $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
 
