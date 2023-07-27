c05d07fd350c604e725e44bc1d17d86d131d3f77
Nishant Kumar <www.rajsonu13@gmail.com>
2023-07-27 15:07:13 +0530

    RMX3031:Sepolicy: Address neverallows
    * Also remove ignore neverallows flag now
    
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -176,7 +176,6 @@ include device/mediatek/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS  += $(DEVICE_PATH)/sepolicy/vendor
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Touch
 SOONG_CONFIG_NAMESPACES += OPLUS_LINEAGE_TOUCH_HAL
