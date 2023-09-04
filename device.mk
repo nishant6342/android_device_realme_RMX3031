6e20fdb85edf88da9286796a74c495a19cbee7b1
bengris32 <bengris32@protonmail.ch>
2023-09-04 22:52:45 +0100

    RMX3031: Switch to Pixel thermal service
    * Config based off "dumpsys thermalservice" using stock thermal service.
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: I92fcc16ce01aae8b1b9307c38f4236dffb7f78b9
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- device.mk
+++ device.mk
@@ -474,8 +474,10 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0.vendor \
-    android.hardware.thermal@1.0-impl
+    android.hardware.thermal@2.0-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # Touch
 PRODUCT_PACKAGES += \
