4748d4d611404e50edc6afe74eceea121d0018b0
SamarV-121 <samarvispute121@pm.me>
2023-09-08 15:41:30 +0530

    RMX3031: Implement DT2W hint
    Change-Id: Icaac26e4d5c3423490ac5589334fe6de34012e19
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- device.mk
+++ device.mk
@@ -134,10 +134,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.common-V2-ndk.vendor \
     disable_configstore
 
-# DT2W
-PRODUCT_PACKAGES += \
-    DT2W-Service-RMX3031
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
