af8124a3040780b8112a8f454b93b1cb5c2114d4
Nishant Kumar <www.rajsonu13@gmail.com>
2023-09-21 13:39:40 +0530

    RMX3031: Build renderscript hal
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- device.mk
+++ device.mk
@@ -379,6 +379,10 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio@1.6.vendor
 
+# RenderScript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RcsService
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
