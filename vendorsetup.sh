087d2f5cc919bee14292f77ac6636930608dccb5
Nishant Kumar <www.rajsonu13@gmail.com>
2023-10-18 16:01:19 +0530

    RMX3031: Don't clone vendor with depth
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,7 +16,7 @@ if [ -n "${CLEAN_DT_REPOS}" ]; then
 fi
 echo start cloning repos
 VT=vendor/realme/RMX3031/RMX3031-vendor.mk
-if ! [ -a $VT ]; then git clone --depth=1 https://github.com/nishant6342/vendor_realme_RMX3031 -b RMUI4-OSS vendor/realme/RMX3031
+if ! [ -a $VT ]; then git clone https://github.com/nishant6342/vendor_realme_RMX3031 -b RMUI4-OSS vendor/realme/RMX3031
 fi
 KT=kernel/realme/RMX3031/Makefile
 if ! [ -a $KT ]; then git clone --depth=1 https://github.com/nishant6342/kernel_realme_RMX3031 -b T kernel/realme/RMX3031
