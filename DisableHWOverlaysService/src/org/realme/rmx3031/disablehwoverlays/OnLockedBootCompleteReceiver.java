53ed42c3cd6869e6df7656b4a6c67c9b107aa1fe
Nishant Kumar <www.rajsonu13@gmail.com>
2023-10-19 17:17:52 +0530

    RMX3031: DisableHWOverlaysService: Directly disable hw overlays on boot without starting service for it
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- DisableHWOverlaysService/src/org/realme/rmx3031/disablehwoverlays/OnLockedBootCompleteReceiver.java
+++ DisableHWOverlaysService/src/org/realme/rmx3031/disablehwoverlays/OnLockedBootCompleteReceiver.java
@@ -4,16 +4,38 @@ import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.Context;
 
+import android.app.Service;
+import android.os.ServiceManager;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
 import android.util.Log;
 
 public class OnLockedBootCompleteReceiver extends BroadcastReceiver {
     private static final String TAG = "DisableHWOverlays";
+    private IBinder mSurfaceFlinger;
 
     @Override
     public void onReceive(final Context context, Intent intent) {
         Log.i(TAG, "onBoot");
+        mSurfaceFlinger = ServiceManager.getService("SurfaceFlinger");
+        disableHardwareOverlaysSetting();
+    }
 
-        Intent sIntent = new Intent(context, DisableHWOverlaysService.class);
-        context.startService(sIntent);
+    void disableHardwareOverlaysSetting() {
+        if (mSurfaceFlinger == null) {
+            Log.e(TAG, "mSurfaceFlinger is null! Aborting");
+            return;
+        }
+        try {
+            final Parcel data = Parcel.obtain();
+            data.writeInterfaceToken("android.ui.ISurfaceComposer");
+            data.writeInt(1);
+            mSurfaceFlinger.transact(1008, data,
+                    null /* reply */, 0 /* flags */);
+            data.recycle();
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to disable HW overlays\n" + ex.toString());
+        }
     }
 }
